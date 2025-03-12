46b1cccb858334e3be9df27b32429f32a8963abd
VitmiTor <torremvictor@gmail.com>
2025-03-11 21:09:23 -0500

    fix all locators

--- src/test/java/firstTest.java
+++ src/test/java/firstTest.java
@@ -1,25 +1,39 @@
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.ElementHandle;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+
+import java.util.List;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 
 public class firstTest {
     public static void main(String[] args) {
         try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
             Page page = browser.newPage();
             page.navigate("https://www.saucedemo.com/v1/");
 
 
-            page.locator("id=user-name").fill("standard_user");
+            page.locator("#user-name").fill("standard_user");
 
-            page.locator("id=password").fill("secret_sauce");
+            page.locator("#password").fill("secret_sauce");
 
             page.locator("id=login-button").click();
 
             page.locator("text=Products").isVisible();
 
+
+            List<ElementHandle> elementHandles = page.querySelectorAll("button[class='btn_primary btn_inventory']");
+
+            
+            for (int i = 0; i < 6; i++) {
+                elementHandles.get(i).click();
+            }
+
+            assertThat(page.locator("span[class='fa-layers-counter shopping_cart_badge']")).containsText("6");
 
         }
     }
